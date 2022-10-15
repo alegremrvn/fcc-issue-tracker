f8d5ad5efde8e38bc930e82bc390940a71e291e5
Marvin Alegre <alegremrvn@protonmail.com>
2022-10-15 08:47:29 +0800

    test: add test #1

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,24 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+  this.timeout(1000)
+  suite('POST /api/issues/:project', function() {
+    // #1
+    test('create an issue with every field', function() {
+      chai
+        .request(server)
+        .post('/api/issues/ourlibraries')
+        .type('form')
+        .send({
+          issue_title: 'banner not centered',
+          issue_text: 'banner not centered',
+          created_by: 'mj',
+          assigned_to: 'mj',
+          status_text: 'in QA'
+        })
+        .end(function(err, res) {
+          assert.isOk(res.body.insertedId)
+        })
+    })
+  })
 });
