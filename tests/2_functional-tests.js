af09ac90f9aa9177661fb55052127e0417912a0b
Marvin Alegre <alegremrvn@protonmail.com>
2022-10-17 15:30:05 +0800

    test: add test #7 & #8

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,12 +1,49 @@
+require('dotenv').config()
 const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
+const { ObjectId } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient
 
 chai.use(chaiHttp);
 
+let URI = process.env.MONGO_URI
+const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 suite('Functional Tests', function() {
-  this.timeout(3000)
+  this.timeout(15000)
+  this.beforeAll(function() {
+    async function run() {
+      try {
+        await client.connect()
+        const projectIssues = client.db('issue_tracker').collection('ourlibraries')
+        let docs = [
+          {
+            _id: new ObjectId('634b7306d1852520da835db1'),
+            created_by: 'asdf',
+            open: true
+          }, 
+          {
+            _id: new ObjectId('634b7306d1852520da835db2'),
+            created_by: 'asdf',
+            open: true
+          },
+          {
+            _id: new ObjectId('634b7306d1852520da835db3'),
+            created_by: 'asdf',
+            open: true
+          }
+        ]
+        await projectIssues.insertMany(docs)
+      } finally {
+        await client.close()
+      }
+    }
+
+    run().catch(console.dir)
+  })
+
   suite('POST /api/issues/:project', function() {
     // #1
     test('#1 create an issue with every field', function(done) {
@@ -97,6 +134,39 @@ suite('Functional Tests', function() {
             assert.equal(res.body[i].created_by, 'mj')
             assert.equal(res.body[i].open, false)
           }
+          done()
+        })
+    })
+  })
+
+  suite('PUT /api/issues/:project', function() {
+    test('#7 update one field on an issue', function(done) {
+      chai
+        .request(server)
+        .put('/api/issues/ourlibraries')
+        .type('form')
+        .send({
+          _id: '634b7306d1852520da835db1',
+          created_by: 'MJ'
+        })
+        .end(function(err, res) {
+          assert.equal(res.body.modifiedCount, 1)
+          done()
+        })
+    })
+
+    test('#8 update multiple fields on an issue', function(done) {
+      chai
+        .request(server)
+        .put('/api/issues/ourlibraries')
+        .type('form')
+        .send({
+          _id: '634b7306d1852520da835db2',
+          created_by: 'MJ',
+          open: false
+        })
+        .end(function(err, res) {
+          assert.equal(res.body.modifiedCount, 1)
           done()
         })
     })
