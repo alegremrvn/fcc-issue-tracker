fc6017b8e673ad87a03053ff7a042d442c58ce1b
Marvin Alegre <alegremrvn@protonmail.com>
2023-02-18 22:08:16 +0800

    test: add PUT, DELETE /api/issues/{project} tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -3,9 +3,9 @@ const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
 
-const { ObjectId } = require('mongodb')
-
 chai.use(chaiHttp);
+
+let mlID
 
 describe('Functional Tests', function () {
   // clean up before running all the tests
@@ -117,6 +117,149 @@ describe('Functional Tests', function () {
           assert.equal(res.body.length, 1)
           assert.equal(res.body[0].created_by, 'Mario')
           assert.equal(res.body[0].assigned_to, 'Luigi')
+
+          done()
+        })
+    })
+  })
+
+  describe('PUT /api/issues/{project}', function () {
+    before(function (done) {
+      chai
+        .request(server)
+        .get('/api/issues/foo')
+        .end(function (err, res) {
+          if (res.body[0].created_by == 'Mario' &&
+            res.body[0].assigned_to == 'Luigi') {
+            mlID = res.body[0]._id
+          } else {
+            mlID = res.body[1]._id
+          }
+
+          done()
+        })
+    })
+
+    it('Test #7) update one field on an issue', function (done) {
+      chai
+        .request(server)
+        .put('/api/issues/foo')
+        .send({
+          _id: mlID,
+          assigned_to: 'Lea'
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.result, 'successfully updated')
+          assert.equal(res.body._id, mlID)
+
+          done()
+        })
+    })
+
+    it('Test #8) update multiple fields on an issue', function (done) {
+      chai
+        .request(server)
+        .put('/api/issues/foo')
+        .send({
+          _id: mlID,
+          created_by: 'Maria',
+          assigned_to: 'Luigi'
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.result, 'successfully updated')
+          assert.equal(res.body._id, mlID)
+
+          done()
+        })
+    })
+
+    it('Test #9) update an issue with missing _id', function (done) {
+      chai
+        .request(server)
+        .put('/api/issues/foo')
+        .send({
+          created_by: 'Maria',
+          assigned_to: 'Luigi'
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.error, 'missing _id')
+
+          done()
+        })
+    })
+
+    it('Test #10) update an issue with no fields to update', function (done) {
+      chai
+        .request(server)
+        .put('/api/issues/foo')
+        .send({
+          _id: mlID,
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.error, 'no update field(s) sent')
+          assert.equal(res.body._id, mlID)
+
+          done()
+        })
+    })
+
+    it('Test #11) update an issue with an invalid _id', function (done) {
+      chai
+        .request(server)
+        .put('/api/issues/foo')
+        .send({
+          _id: 'bar',
+          created_by: 'Steve'
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.error, 'could not update')
+          assert.equal(res.body._id, 'bar')
+
+          done()
+        })
+    })
+  })
+
+  describe('DELETE /api/issues/{project}', function () {
+    it('Test #12) delete an issue', function (done) {
+      chai
+        .request(server)
+        .delete('/api/issues/foo')
+        .send({
+          _id: mlID
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.result, 'successfully deleted')
+          assert.equal(res.body._id, mlID)
+
+          done()
+        })
+    })
+
+    it('Test #13) delete an issue with an invalid _id', function (done) {
+      chai
+        .request(server)
+        .delete('/api/issues/foo')
+        .send({
+          _id: 'bar'
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.error, 'could not delete')
+          assert.equal(res.body._id, 'bar')
+
+          done()
+        })
+    })
+
+    it('Test #14) delete an issue with missing _id', function (done) {
+      chai
+        .request(server)
+        .delete('/api/issues/foo')
+        .send({
+          created_by: 'Mario'
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.error, 'missing _id')
 
           done()
         })
